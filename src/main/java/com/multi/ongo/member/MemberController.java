a214d6aa423a2f29e50c814721d80595d96647b2
PSJ <psjsub1@gmail.com>
2022-12-30 00:49:32 +0900

    멤버 디티오

--- src/main/java/com/multi/ongo/member/MemberController.java
+++ src/main/java/com/multi/ongo/member/MemberController.java
@@ -3,10 +3,13 @@ package com.multi.ongo.member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@SessionAttributes("user")
 public class MemberController {
+	
 	
 		/*
 		@RequestMapping(value="/member/login",method = RequestMethod.GET)
@@ -34,10 +37,23 @@ public class MemberController {
 				</span>
 			</c:otherwise>
 			
-		*/
 			//서비스 겟방식으로 로그인뷰 보기
 			@RequestMapping(value="member/login",method = RequestMethod.GET)
 			public String empservicewritePage() {
+				return "member/logout";
+			}
+			//포스트방식으로 글 등록
+			@RequestMapping(value = "member/logout",method = RequestMethod.POST)
+			public String write(MemberDTO board) {
+				//실제 db에 저장할때 호출되는 컨트롤러의 메소드
+				return "redirect:/index.do";
+			}
+			
+		*/
+	
+			//서비스 겟방식으로 로그인뷰 보기
+			@RequestMapping(value="member/login")
+			public String springlogin() {
 				return "member/logout";
 			}
 			//포스트방식으로 글 등록
--- src/main/java/com/multi/ongo/member/MemberDTO.java
+++ src/main/java/com/multi/ongo/member/MemberDTO.java
@@ -2,45 +2,48 @@ package com.multi.ongo.member;
 import java.sql.Date;
 
 public class MemberDTO {
-	private String member_id; //회원아이디
+	private String member_id; //회원아이디 primary key
 	private int member_no; //회원번호
 	private String member_pw; // 비밀번호
 	private String member_name; //이름
-	private String member_addr1; // 주소 = 도로명주소
-	private String member_addr2; // 상세주소
-	private String member_post; //우편번호
+	private String member_addr; // 주소 = 도로명주소
+	private String zipcode; //우편번호
 	private String member_phone; //휴대폰번호
 	private String member_email; //이메일주소
 	private String member_role; //회원권한 (일반/관리자)
-	private Date reg_date; // 가입일
+	private Date member_date; // 가입일
+	private String member_sign; //가입상태
 	public MemberDTO() {
 		
 	}
 	
-	public MemberDTO(String member_id, int member_no, String member_pw, String member_name, String member_addr1,
-			String member_addr2, String member_post, String member_phone, String member_email, String member_role,
-			Date reg_date) {
+	
+	public MemberDTO(String member_id, int member_no, String member_pw, String member_name, String member_addr,
+			String zipcode, String member_phone, String member_email, String member_role, Date member_date,
+			String member_sign) {
 		super();
 		this.member_id = member_id;
 		this.member_no = member_no;
 		this.member_pw = member_pw;
 		this.member_name = member_name;
-		this.member_addr1 = member_addr1;
-		this.member_addr2 = member_addr2;
-		this.member_post = member_post;
+		this.member_addr = member_addr;
+		this.zipcode = zipcode;
 		this.member_phone = member_phone;
 		this.member_email = member_email;
 		this.member_role = member_role;
-		this.reg_date = reg_date;
+		this.member_date = member_date;
+		this.member_sign = member_sign;
 	}
-	
+
+
 	@Override
 	public String toString() {
 		return "MemberDTO [member_id=" + member_id + ", member_no=" + member_no + ", member_pw=" + member_pw
-				+ ", member_name=" + member_name + ", member_addr1=" + member_addr1 + ", member_addr2=" + member_addr2
-				+ ", member_post=" + member_post + ", member_phone=" + member_phone + ", member_email=" + member_email
-				+ ", member_role=" + member_role + ", reg_date=" + reg_date + "]";
+				+ ", member_name=" + member_name + ", member_addr=" + member_addr + ", zipcode=" + zipcode
+				+ ", member_phone=" + member_phone + ", member_email=" + member_email + ", member_role=" + member_role
+				+ ", member_date=" + member_date + ", member_sign=" + member_sign + "]";
 	}
+
 
 	public String getMember_id() {
 		return member_id;
@@ -66,23 +69,17 @@ public class MemberDTO {
 	public void setMember_name(String member_name) {
 		this.member_name = member_name;
 	}
-	public String getMember_addr1() {
-		return member_addr1;
+	public String getMember_addr() {
+		return member_addr;
 	}
-	public void setMember_addr1(String member_addr1) {
-		this.member_addr1 = member_addr1;
+	public void setMember_addr(String member_addr) {
+		this.member_addr = member_addr;
 	}
-	public String getMember_addr2() {
-		return member_addr2;
+	public String getZipcode() {
+		return zipcode;
 	}
-	public void setMember_addr2(String member_addr2) {
-		this.member_addr2 = member_addr2;
-	}
-	public String getMember_post() {
-		return member_post;
-	}
-	public void setMember_post(String member_post) {
-		this.member_post = member_post;
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
 	}
 	public String getMember_phone() {
 		return member_phone;
@@ -102,13 +99,19 @@ public class MemberDTO {
 	public void setMember_role(String member_role) {
 		this.member_role = member_role;
 	}
-	public Date getReg_date() {
-		return reg_date;
+	public Date getMember_date() {
+		return member_date;
 	}
-	public void setReg_date(Date reg_date) {
-		this.reg_date = reg_date;
+	public void setMember_date(Date member_date) {
+		this.member_date = member_date;
 	}
-	
+	public String getMember_sign() {
+		return member_sign;
+	}
+	public void setMember_sign(String member_sign) {
+		this.member_sign = member_sign;
+	}
+
 	
 	
 }
