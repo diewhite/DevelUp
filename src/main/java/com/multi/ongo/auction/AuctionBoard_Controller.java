c892e2e40668f44fc0551d90234589db74ab4be3
Kim Gyumin <diewhite@icloud.com>
2023-01-14 13:56:49 +0900

    - auction list에서 read 연결

--- src/main/java/com/multi/ongo/auction/AuctionBoard_Controller.java
+++ src/main/java/com/multi/ongo/auction/AuctionBoard_Controller.java
@@ -60,20 +60,14 @@ public class AuctionBoard_Controller {
 		}
 	//경매게시판 글 읽기
 	@RequestMapping("/auction/auctionRead") 
-	public String auctionRead(int auction_no, String status, Model model) {
-		String view = "";
+	public String auctionRead(int auction_no, Model model) {
 		AuctionBoard_DTO board = service.auctionRead(auction_no);		
-		System.out.println("no:"+auction_no+",status:"+status);
+		System.out.println("no:"+auction_no);
 		System.out.println("start:"+board.getWrite_date());
 		System.out.println("end:"+board.getEnd_date());
-		model.addAttribute("board", board);		
-		if(status.equals("READ")) {
-			view = "auctionRead";
-			System.out.println(board);
-		}else {
-			view = "auctionUpdate";
-		}
-		return view;
+		model.addAttribute("board", board);			
+
+		return "auctionRead";
 		}
 	
 }
